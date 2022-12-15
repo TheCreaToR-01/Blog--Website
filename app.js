80096c95638a2ada8b02ce4ffde70ba21642d196
KUSHAGRA DUBEY <greatnamokushagra@gmail.com>
2022-12-15 09:29:13 -0800

    Initial Commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+const theTruncate = require("truncate");
 const blogs = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -25,7 +27,18 @@ app.get("/", function (req, res) {
 });
 
 app.get("/blogs/:title", function(req, res){
-  console.log(req.title);
+  const requestedTitle = req.params.title;
+
+  blogs.forEach(function(blog){
+    let savedTitle = blog.title;
+
+    if (_.kebabCase(savedTitle) === _.kebabCase(requestedTitle)){
+      res.render('blog.ejs', {pageTitle : savedTitle, pageContent: blog.content});
+    }
+    else{
+      console.log("Not a Match.");
+    }
+  })
 })
 
 app.get("/about", function (req, res) {
